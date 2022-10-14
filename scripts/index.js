fa9e6fd21321fdeda567045ef5052c7a2bec166e
CarlosEduardo-galleth <carlospcavalari@gmail.com>
2022-10-14 17:43:41 -0300

    gitsearch-carlos

--- scripts/index.js
+++ scripts/index.js
@@ -6,7 +6,7 @@ async function getData() {
     const submitBtn = document.getElementById("submit")
     const baseUrl = "https://api.github.com/users"
 
-    await submitBtn.addEventListener('click', () => {
+    submitBtn.addEventListener('click', () => {
         fetch(`${baseUrl}/${inputSearch.value}`)
         .then(elem => elem.json())
         .then(elem => searchIsValid(elem))
@@ -22,13 +22,19 @@ function buttonAnimation(btn){
     btn.style.backgroundColor = "rgba(214, 51, 108, 0.4)"
     btn.style.cursor = "default"
     btn.append(div)
+    
+    setTimeout(() => {
+        btn.innerText = "Ver perfil do github"
+        btn.style.backgroundColor = "var(--color-brand-2)"
+        btn.style.cursor = "pointer"
+    }, 2000)
+
 }
 function searchIsValid(elem) {
     const notFoundAlert = document.getElementsByClassName("not-found")[0]
     const inputSearch = document.getElementById("search-user")
-    
     if (elem.message == "Not Found") {
-        notFoundAlert.classList.remove("dont-show")
+        notFoundAlert.classList.remove("dont-show");
     }
     else{
         notFoundAlert.classList.add("dont-show")
@@ -41,14 +47,22 @@ function searchIsValid(elem) {
         notFoundAlert.classList.add("dont-show") 
     }
     return elem
-} 
+}
 function localStor(elem) {
+    let arrayFounded = []
     const foundedList = document.getElementsByClassName("founded-recently-list")[0]
     let data = localStorage.getItem("usuario-recente")
     if (data) {
         let img = document.createElement("img")
         img.src = elem.avatar_url
-        foundedList.append(img)
+
+        arrayFounded.push(img)
+        arrayFounded.forEach(element => {
+            foundedList.append(element)
+        })
+    }
+    if (foundedList.children.length > 3) {
+        foundedList.children[0].remove()
     }
 }
 
@@ -62,5 +76,4 @@ function renderizarEncontrados() {
     }
 }
 renderizarEncontrados()
-
 getData()
